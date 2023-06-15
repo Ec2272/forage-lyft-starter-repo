1c54807da349a9c60f84cd7553bde5dd174c64ee
Ec2272 <125344533+Ec2272@users.noreply.github.com>
2023-06-15 04:27:09 +0100

    Create Spindler.py

---
+++ test/Spindler.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from abc import ABC, abstractmethod
+
+from test.Servicable import servicable
+from car import Car
+from Battery import Battery
+
+class SpindlerBattery (Battery):
+    def __init__(self, battery, engine, service_date, current date):
+        self.battery = Car(battery)
+        self.engine = Car(engine)
+        self.service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        self.service_date = self.last_service_date.
+        self.current_date = datetime.today().date()
+
+
+    @abstractmethod
+    def needs_service(self):
+        pass
