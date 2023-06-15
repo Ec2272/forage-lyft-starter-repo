0087d156aa4b42cab8db45ce437b1d57025a0306
Ec2272 <125344533+Ec2272@users.noreply.github.com>
2023-06-15 04:29:17 +0100

    Create engine.py

---
+++ test/engine.py
@@ -0,0 +1,9 @@
+from abc import ABC
+
+from car import Car
+class Engine(Car):
+    
+    def __init__(self, battery, engine,):
+        self.battery = Car(battery)
+        self.engine = Car(engine)
+        
